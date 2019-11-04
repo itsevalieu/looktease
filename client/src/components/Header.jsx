27a65b6239a19d4ea268a81848da7c65089f3530
itsevalieu <itsevalieu@gmail.com>
2019-11-03 22:11:27 -0800

    Refactoring Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,20 +1,59 @@
 import React from 'react';
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import site_logo from './site-logo.png';
+import { h1, a } from '../styles';
 
 const StyledHeader = styled.header`
-    background: rgb(169, 210, 243);
-    height: 80px;
     display: flex;
     flex-flow: row nowrap;
+    height: 80px;
+    align-items: center;
+    align-content: center;
     justify-content: space-between;
-    align-items: flex-end;
+    padding: 0 18px;
+    img {
+        width: 40px;
+        height: 35px;
+        margin: 5px 10px;
+    }
+    h1 {
+        text-align: center;
+        font-size: ${h1};
+        font-weight: 100;
+        font-family: 'Dosis', sans-serif;
+    }
+    a {
+        text-decoration: none;
+        color: #333333;
+    }
+    ul {
+        display: flex;
+        width: 100%;
+        justify-content: flex-end;
+        align-items: center;
+        align-content: center;
+        a {
+            margin: 0px 20px;
+            white-space: nowrap;
+            font-size: ${a};
+            font-family: 'Dosis', sans-serif;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+    }
 `;
 
 export default function Header() {
     return (
         <StyledHeader>
-            <h1>LookTease</h1>
+            <div>
+                <Link to='/'><img src={site_logo} alt='Looktease Logo' /></Link>
+            </div>
+            <div>
+                <Link to='/'><h1>looktease</h1></Link>
+            </div>
             <nav>
                 <ul>
                     <NavLink to='/'>Home</NavLink>
---
+++ client/src/styles/index.js
@@ -0,0 +1,9 @@
+// Fonts
+export const h1 = '2.0em';
+export const h2 = '1.7em';
+export const h3 = '1.65em';
+export const h4 = '1.3em';
+export const h5 = '1.2em';
+export const h6 = '1.1em';
+export const p = '1.2em';
+export const a = '1.1em';
